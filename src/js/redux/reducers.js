aff07dc99322194be89b64ced031303e54a5a65e
lyctea <396136337@qq.com>
2017-02-22 16:14:15 +0800

    reducers
    状态初始化

---
+++ src/js/redux/reducers.js
@@ -0,0 +1,93 @@
+import { PLAYERSTEATESHIFT,REQUESTSONG,RECEIVESONG,SONGTIMEUPDATE,VOLUMEUPDATE,CHANGEPLAYMODE } from '../Constants/ActionType.js';
+import { REQUESTLYRIC,RECEIVELYRIC,FAILLYRIC } from '../Constants/ActionType.js';
+import { REQUESTCHANNELLIST,RECEIVECHANNELLIST,FAILQUERYCHANNELLIST} from '../Constants/ActionType.js';
+import { UPDATEPLAYLIST,UPDATEPLAYLISTINDEX,CLEARALLPLAYLIST,SHOWPLAYLIST,UPDATEPLAYLISTSONG } from '../Constants/ActionType.js';
+import { UPDATELOCALLIST,DELETEFROMLOCALLIST } from '../Constants/ActionType.js';
+import { KEYWORDCHANGE,REQUESTKEYWORDQUERY,RECEIVEKEYWORDQUERY,KEYWORDQUERYFAIL } from '../Constants/ActionType.js';
+import objectAssign from 'object-assign';
+import { StorageGetter,getIndex} from '../util/tool.js';
+
+
+const initMusicState = StorageGetter('musicState') || {
+        playFlag: false,
+        volume: 0.3,
+        isFetching: false,
+        currentTime: 0,
+        song_id: -1,
+        title: "",
+        author: "",
+        album_title:"",
+        file_duration: 0,
+        pic_small: "",
+        pic_big: "",
+        song_url: "",
+        isFetchingLyric: false,
+        lrcContent: [['00:00','选择下一首歌欣赏下']],
+    };
+
+const initLocalPlayList = StorageGetter('localPlayList') || {
+        length: 0,
+        date: '2017-01-01',
+        name: '我喜欢的音乐',
+        comment: '',
+        avator_url: '',
+        song_list: []
+    };
+
+const initCurPlayList = StorageGetter('curPlayList') || {
+        isDisplay: false,
+        length: 0,
+        mode: 0,
+        curIndex: -1,
+        song_list:[]
+    };
+const initChannelPlayList = {
+        isFetching: false,
+        success: true
+}
+const initKeyWordSearchList = {
+        keyword: '',
+        isFetching: false,
+        length: 0,
+        success: true,
+        result: []
+}
+
+//播放器Reducer,musicState
+export const musicState = (preState = initMusicState,action) => {
+        switch (action.type){
+            case PLAYERSTEATESHIFT:
+                    return objectAssign({},preState,{playFlag:action.playFlag});
+            case VOLUMEUPDATE:{
+                    return objectAssign({},preState,{volume:action.volume});
+            }
+            case SONGTIMEUPDATE:{
+                    return objectAssign({},preState,{currentTime:action.currentTime});
+            }
+            case REQUESTSONG:{
+                    return objectAssign({},preState,{playFlag:false,
+                                                     isFetching:action.isFetching,
+                                                     song_id:action.fetchSongId});
+            }
+            case RECEIVESONG:{
+                    if (preState.song_id !== action.fetchSongId){
+                            return preState;
+                    }
+                    return objectAssign({},preState,{
+                            isFetching:action.isFetching,
+                        song_id:action.fetchSongId,
+                        currentTime:0,
+                        title:action.item.songinfo.titile,
+                        author:action.item.songinfo.author,
+                        album_title: action.item.songinfo.album_title,
+                        file_duration: action.item.bitrate.file_duration,
+                        pic_small: action.item.songinfo.pic_small,
+                        pic_big: action.item.songinfo.pic_big,
+                        song_url: action.item.bitrate.show_link,
+                        lrclink: action.item.songinfo.lrclink,
+                    });
+            }
+            case REQUESTLYRIC:
+                    return objectAssign({},)
+        }
+}
