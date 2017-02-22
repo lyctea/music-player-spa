1b4bce845676f25042d50a3307fa70825ae5bea6
lyctea <396136337@qq.com>
2017-02-22 18:41:39 +0800

    播放列表reducer

--- src/js/redux/reducers.js
+++ src/js/redux/reducers.js
@@ -159,3 +159,46 @@ export const channelPlayList = (preState=initChannelPlayList,action) => {
         }
 }
 
+//播放列表reducer
+export const curPlayList = (preState = initCurPlayList,action) => {
+        switch (action.type){
+            case UPDATEPLAYLIST:
+                    if (action.operation === 'REPLACE'){
+                            return objectAssign({},preState,{length:action.items.length,
+                                                        curIndex:0,
+                                                        song_list: action.items});
+                    }else if(action.operation === 'ADD'){
+                        return objectAssign({},preState,{
+                            length: preState.length+action.items.length,
+                            //这里不能用preState.song_list.push.apply(,action.items).因为push操作，只修改原数组，返回length。
+                            //而我们这里需要返回一个新的数组，所以用 concat
+                            song_list: preState.song_list.concat(action.items)
+                        });
+                    }
+            case UPDATEPLAYLISTINDEX:
+                    return objectAssign({},preState,{
+                            curIndex: action.curIndex
+                    });
+            case UPDATEPLAYLISTSONG:
+                    console.log(action.song_id,preState.song_list);
+                    return objectAssign({},preState,{
+                            curIndex: getIndex('song_id',{'song_id':action.song_id},preState.song_list)
+                    });
+            case CHANGEPLAYMODE:
+                    return objectAssign({},preState,{
+                            mode: (preState.mode+1)%2
+                    });
+            case CLEARALLPLAYLIST:
+                    return objectAssign({},preState,{
+                            length: 0,
+                            curIndex: -1,
+                        song_list:[]
+                    });
+            case SHOWPLAYLIST:
+                    return objectAssign({},preState,{
+                            isDisplay:!preState.isDisplay
+                    });
+            default:
+                    return preState
+        }
+}
